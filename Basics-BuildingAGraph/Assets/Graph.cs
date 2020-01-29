e96379a21e1548649370e6cfb1c55f840ea98f60
RunninglVlan <redcapshortcut@gmail.com>
2020-01-29 16:09:33 +0200

    Started adding points to Graph, now Clearing old ones before Visualizing

--- Basics-BuildingAGraph/Assets/Graph.cs
+++ Basics-BuildingAGraph/Assets/Graph.cs
@@ -12,15 +12,22 @@ public class Graph : MonoBehaviour {
 
     [Button]
     void Visualize() {
+        Clear();
         var step = 2f / resolution;
         var scale = Vector3.one * step;
         var position = Vector3.zero;
         foreach (var index in Enumerable.Range(-resolution / 2, resolution)) {
-            var point = Instantiate(pointPrefab);
+            var point = Instantiate(pointPrefab, transform);
             point.localScale = scale;
             position.x = (index + .5f) * step;
             position.y = Mathf.Pow(position.x, 2);
             point.localPosition = position;
+        }
+
+        void Clear() {
+            foreach (Transform child in transform) {
+                Destroy(child.gameObject);
+            }
         }
     }
 }
