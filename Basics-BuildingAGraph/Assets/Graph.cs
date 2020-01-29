7bced2421fff5f8529a502be455fcb59161a57b8
RunninglVlan <redcapshortcut@gmail.com>
2020-01-29 15:55:13 +0200

    Extracted resolution to SerializeField with Range
    Named logic in Awake, added NaughtyAttributes' Button

--- Basics-BuildingAGraph/Assets/Graph.cs
+++ Basics-BuildingAGraph/Assets/Graph.cs
@@ -1,14 +1,20 @@
-﻿using System.Linq;
+﻿using NaughtyAttributes;
+using System.Linq;
 using UnityEngine;
 
 public class Graph : MonoBehaviour {
 
     [SerializeField] Transform pointPrefab = default;
+    [Range(10, 100)]
+    [SerializeField] int resolution = 10;
 
-    void Awake() {
+    void Awake() => Visualize();
+
+    [Button]
+    void Visualize() {
         var scale = Vector3.one / 5;
         var position = Vector3.zero;
-        foreach (var index in Enumerable.Range(-5, 10)) {
+        foreach (var index in Enumerable.Range(-5, resolution)) {
             var point = Instantiate(pointPrefab);
             point.localScale = scale;
             position.x = (index + .5f) / 5;
