6ac27401eb093915c778cb01fee507d7b44ff423
RunninglVlan <redcapshortcut@gmail.com>
2020-01-29 15:14:45 +0200

    Positioned 10 small cubes in x range (-1, 1)

--- Basics-BuildingAGraph/Assets/Graph.cs
+++ Basics-BuildingAGraph/Assets/Graph.cs
@@ -6,9 +6,10 @@ public class Graph : MonoBehaviour {
     [SerializeField] Transform pointPrefab = default;
 
     void Awake() {
-        foreach (var index in Enumerable.Range(0, 10)) {
+        foreach (var index in Enumerable.Range(-5, 10)) {
             var point = Instantiate(pointPrefab);
-            point.localPosition = Vector3.right * index;
+            point.localPosition = Vector3.right * (index + .5f) / 5;
+            point.localScale = Vector3.one / 5;
         }
     }
 }
