255c22fa81b929c01189f8a7e7587142d0d73747
RunninglVlan <redcapshortcut@gmail.com>
2020-01-29 15:31:57 +0200

    Smile f(x) = x^2 function

--- Basics-BuildingAGraph/Assets/Graph.cs
+++ Basics-BuildingAGraph/Assets/Graph.cs
@@ -7,11 +7,12 @@ public class Graph : MonoBehaviour {
 
     void Awake() {
         var scale = Vector3.one / 5;
-        var position = Vector3.right;
+        var position = Vector3.zero;
         foreach (var index in Enumerable.Range(-5, 10)) {
             var point = Instantiate(pointPrefab);
             point.localScale = scale;
             position.x = (index + .5f) / 5;
+            position.y = Mathf.Pow(position.x, 2);
             point.localPosition = position;
         }
     }
