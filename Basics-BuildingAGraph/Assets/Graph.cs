d73700fd20ed418ea36e7e093fed41127893626d
RunninglVlan <redcapshortcut@gmail.com>
2020-01-29 13:50:27 +0200

    Playing with cube instantiation

--- Basics-BuildingAGraph/Assets/Graph.cs
+++ Basics-BuildingAGraph/Assets/Graph.cs
@@ -1,5 +1,14 @@
 ﻿using UnityEngine;
 
 public class Graph : MonoBehaviour {
-    [SerializeField] Transform pointPrefab;
+
+    [SerializeField] Transform pointPrefab = default;
+
+    void Awake() {
+        var point = Instantiate(pointPrefab);
+        point.localPosition = Vector3.right;
+
+        point = Instantiate(pointPrefab);
+        point.localPosition = Vector3.right * 2;
+    }
 }
