d9ba21e5079fda8a3ea93f2329e84454b2a9bf5a
RunninglVlan <redcapshortcut@gmail.com>
2020-02-11 13:08:15 +0200

    Updated Information to support Controls and Enabling/Disabling Panel

--- Basics-MathematicalSurfaces/Assets/Utils/Information.cs
+++ Basics-MathematicalSurfaces/Assets/Utils/Information.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
 
 public class Information : MonoBehaviour {
@@ -8,9 +9,11 @@ public class Information : MonoBehaviour {
     [HideInInspector]
     [SerializeField] TextMeshProUGUI textComponent = default;
 
+    [SerializeField] bool panelEnabled = false;
     [SerializeField] Vector2 size = new Vector2(330, 70);
     [Multiline]
     [SerializeField] string text = default;
+    [SerializeField] string[] controls = default;
     [SerializeField] string link = default;
 
     public void bTogglePanel() => panel.gameObject.SetActive(!panel.gameObject.activeSelf);
@@ -18,7 +21,9 @@ public class Information : MonoBehaviour {
     public void bOpenLink() => Application.OpenURL(link);
 
     void OnValidate() {
+        panel.gameObject.SetActive(panelEnabled);
         panel.sizeDelta = size;
-        textComponent.text = text;
+        var joinedControls = String.Join(", ", controls);
+        textComponent.text = string.Join("\n", text, $"Controls: {joinedControls}");
     }
 }
