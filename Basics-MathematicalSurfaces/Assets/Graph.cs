01947eafbc65413503f0948346c246ff0a68953e
RunninglVlan <redcapshortcut@gmail.com>
2020-02-12 16:37:58 +0200

    3.1 Changed return type to be Vector3 instead of float for y

--- Basics-MathematicalSurfaces/Assets/Graph.cs
+++ Basics-MathematicalSurfaces/Assets/Graph.cs
@@ -6,7 +6,7 @@ using static UnityEngine.Mathf;
 
 public class Graph : MonoBehaviour {
 
-    private static readonly Func<float, float, float, float>[] functions = {
+    private static readonly Func<float, float, float, Vector3>[] functions = {
         Sine, Sine2D, MultiSine, MultiSine2D, Ripple
     };
 
@@ -52,35 +52,41 @@ public class Graph : MonoBehaviour {
         var time = Time.time;
         var function = functions[currentFunction];
         foreach (Transform point in transform) {
-            var position = point.position;
-            position.y = function(position.x, position.z, time);
-            point.localPosition = position;
+            point.localPosition = function(point.localPosition.x, point.localPosition.z, time);
         }
     }
 
-    private static float Sine(float x, float _, float time) => Mathf.Sin(PI * (x + time));
+    private static Vector3 Sine(float x, float z, float time) {
+        var y = Sine(x, time);
+        return new Vector3(x, y, z);
+    }
 
-    private static float Sine2D(float x, float z, float time) => (Sine(x, 0, time) + Sine(z, 0, time)) * .5f;
+    private static Vector3 Sine2D(float x, float z, float time) {
+        var y = (Sine(x, time) + Sine(z, time)) * .5f;
+        return new Vector3(x, y, z);
+    }
 
-    private static float MultiSine(float x, float _, float time) {
-        var y = Sine(x, _, time);
+    private static float Sine(float x, float time) => Mathf.Sin(PI * (x + time));
+
+    private static Vector3 MultiSine(float x, float z, float time) {
+        var y = Sine(x, time);
         y += Mathf.Sin(2 * PI * (x + 2 * time)) / 2;
         y *= 2f / 3;
-        return y;
+        return new Vector3(x, y, z);
     }
 
-    private static float MultiSine2D(float x, float z, float time) {
+    private static Vector3 MultiSine2D(float x, float z, float time) {
         var y = 4f * Mathf.Sin(PI * (x + z + time * .5f));
-        y += Sine(x, 0, time);
+        y += Sine(x, time);
         y += Mathf.Sin(2f * PI * (z + 2f * time)) * .5f;
         y *= 1f / 5.5f;
-        return y;
+        return new Vector3(x, y, z);
     }
 
-    private static float Ripple(float x, float z, float time) {
+    private static Vector3 Ripple(float x, float z, float time) {
         var distance = Mathf.Sqrt(x * x + z * z);
         var y = Mathf.Sin(4 * (PI * distance - time));
         y /= 1 + 10 * distance;
-        return y;
+        return new Vector3(x, y, z);
     }
 }
