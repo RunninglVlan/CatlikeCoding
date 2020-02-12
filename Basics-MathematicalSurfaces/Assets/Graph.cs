c46785653e3efc24fa532f6bf38612cb951068f4
RunninglVlan <redcapshortcut@gmail.com>
2020-02-12 16:04:55 +0200

    2.5 Added Ripple function

--- Basics-MathematicalSurfaces/Assets/Graph.cs
+++ Basics-MathematicalSurfaces/Assets/Graph.cs
@@ -6,7 +6,9 @@ using static UnityEngine.Mathf;
 
 public class Graph : MonoBehaviour {
 
-    private static readonly Func<float, float, float, float>[] functions = { Sine, Sine2D, MultiSine, MultiSine2D };
+    private static readonly Func<float, float, float, float>[] functions = {
+        Sine, Sine2D, MultiSine, MultiSine2D, Ripple
+    };
 
     [SerializeField] Transform pointPrefab = default;
     [Range(10, 100)]
@@ -72,6 +74,13 @@ public class Graph : MonoBehaviour {
         y += Sine(x, 0, time);
         y += Mathf.Sin(2f * PI * (z + 2f * time)) * .5f;
         y *= 1f / 5.5f;
+        return y;
+    }
+
+    private static float Ripple(float x, float z, float time) {
+        var distance = Mathf.Sqrt(x * x + z * z);
+        var y = Mathf.Sin(4 * (PI * distance - time));
+        y /= 1 + 10 * distance;
         return y;
     }
 }
