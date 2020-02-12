e0864d74f61148fcafd3ec3050cf1678a43c341f
RunninglVlan <redcapshortcut@gmail.com>
2020-02-12 17:37:52 +0200

    3.1 Added Twisting Cylinder, brought back Button

--- Basics-MathematicalSurfaces/Assets/Graph.cs
+++ Basics-MathematicalSurfaces/Assets/Graph.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NaughtyAttributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -7,24 +8,26 @@ using static UnityEngine.Mathf;
 public class Graph : MonoBehaviour {
 
     private static readonly Func<float, float, float, Vector3>[] functions = {
-        Sine, Sine2D, MultiSine, MultiSine2D, Ripple
+        Sine, Sine2D, MultiSine, MultiSine2D, Ripple, Cylinder
     };
 
     [SerializeField] Transform pointPrefab = default;
     [Range(10, 100)]
     [SerializeField] int resolution = 10;
 
-    private float Step => 2f / resolution;
+    private int Resolution { set; get; }
+    private float Step => 2f / Resolution;
 
     private int currentFunction = 0;
     private Dictionary<(float u, float v), Transform> points = new Dictionary<(float, float), Transform>();
 
     void Awake() => Initialize();
 
-    private void Initialize() {
+    [Button]
+    void Initialize() {
+        Resolution = resolution;
         Clear();
-        var step = Step;
-        var scale = Vector3.one * step;
+        var scale = Vector3.one * Step;
         IteratePoints((u, v) => {
             var point = Instantiate(pointPrefab, transform);
             point.localScale = scale;
@@ -58,9 +61,9 @@ public class Graph : MonoBehaviour {
 
     private void IteratePoints(Action<float, float> action) {
         var step = Step;
-        foreach (var z in Enumerable.Range(-resolution / 2, resolution)) {
+        foreach (var z in Enumerable.Range(-Resolution / 2, Resolution)) {
             var v = (z + .5f) * step;
-            foreach (var x in Enumerable.Range(-resolution / 2, resolution)) {
+            foreach (var x in Enumerable.Range(-Resolution / 2, Resolution)) {
                 var u = (x + .5f) * step;
                 action(u, v);
             }
@@ -76,8 +79,6 @@ public class Graph : MonoBehaviour {
         var y = (Sine(x, time) + Sine(z, time)) * .5f;
         return new Vector3(x, y, z);
     }
-
-    private static float Sine(float x, float time) => Mathf.Sin(PI * (x + time));
 
     private static Vector3 MultiSine(float x, float z, float time) {
         var y = Sine(x, time);
@@ -100,4 +101,12 @@ public class Graph : MonoBehaviour {
         y /= 1 + 10 * distance;
         return new Vector3(x, y, z);
     }
+
+    private static Vector3 Cylinder(float u, float v, float time) {
+        var radius = .8f + Mathf.Sin(PI * (6 * u + 2 * v + time)) * .2f;
+        return new Vector3(radius * Sine(u), v, radius * Cos(u));
+    }
+
+    private static float Sine(float angle, float time = 0) => Mathf.Sin(PI * (angle + time));
+    private static float Cos(float angle, float time = 0) => Mathf.Cos(PI * (angle + time));
 }
