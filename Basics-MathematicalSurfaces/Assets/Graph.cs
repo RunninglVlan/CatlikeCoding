6b6cb381eca19f93c3dc199cf4a90244fd0ad267
RunninglVlan <redcapshortcut@gmail.com>
2020-02-12 17:20:54 +0200

    3.1 Fixed Update as we're now changing full localPosition
    Saving points in Dictionary, setting position every Update

--- Basics-MathematicalSurfaces/Assets/Graph.cs
+++ Basics-MathematicalSurfaces/Assets/Graph.cs
@@ -1,5 +1,5 @@
-﻿using NaughtyAttributes;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using static UnityEngine.Mathf;
@@ -14,25 +14,22 @@ public class Graph : MonoBehaviour {
     [Range(10, 100)]
     [SerializeField] int resolution = 10;
 
+    private float Step => 2f / resolution;
+
     private int currentFunction = 0;
+    private Dictionary<(float u, float v), Transform> points = new Dictionary<(float, float), Transform>();
 
-    void Awake() => Visualize();
+    void Awake() => Initialize();
 
-    [Button]
-    void Visualize() {
+    private void Initialize() {
         Clear();
-        var step = 2f / resolution;
+        var step = Step;
         var scale = Vector3.one * step;
-        var position = Vector3.zero;
-        foreach (var z in Enumerable.Range(-resolution / 2, resolution)) {
-            position.z = (z + .5f) * step;
-            foreach (var x in Enumerable.Range(-resolution / 2, resolution)) {
-                var point = Instantiate(pointPrefab, transform);
-                point.localScale = scale;
-                position.x = (x + .5f) * step;
-                point.localPosition = position;
-            }
-        }
+        IteratePoints((u, v) => {
+            var point = Instantiate(pointPrefab, transform);
+            point.localScale = scale;
+            points[(u, v)] = point;
+        });
 
         void Clear() {
             foreach (Transform point in transform) {
@@ -49,10 +46,24 @@ public class Graph : MonoBehaviour {
     }
 
     private void Animate() {
+        if (!points.Any()) { // Could be removed for build, used only in development
+            Initialize();
+        }
         var time = Time.time;
         var function = functions[currentFunction];
-        foreach (Transform point in transform) {
-            point.localPosition = function(point.localPosition.x, point.localPosition.z, time);
+        IteratePoints((u, v) => {
+            points[(u, v)].localPosition = function(u, v, time);
+        });
+    }
+
+    private void IteratePoints(Action<float, float> action) {
+        var step = Step;
+        foreach (var z in Enumerable.Range(-resolution / 2, resolution)) {
+            var v = (z + .5f) * step;
+            foreach (var x in Enumerable.Range(-resolution / 2, resolution)) {
+                var u = (x + .5f) * step;
+                action(u, v);
+            }
         }
     }
 
