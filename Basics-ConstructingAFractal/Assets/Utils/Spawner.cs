8896467a71b80a01a26345a1efb4f14383020cb2
RunninglVlan <redcapshortcut@gmail.com>
2020-08-03 19:11:06 +0300

    Added Spawner to enable respawning objects, in this case Fractal
    Added it to Utils

---
+++ Basics-ConstructingAFractal/Assets/Utils/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    [SerializeField] GameObject prefab = default;
+
+    GameObject spawned;
+
+    void Start() => Respawn();
+
+    [Button]
+    public void Respawn() {
+        if (spawned) {
+            Destroy(spawned);
+        }
+        spawned = Instantiate(prefab);
+    }
+}
