bf1c086a49131160b07d77ecf1d070ad7847f8ed
RunninglVlan <redcapshortcut@gmail.com>
2020-08-03 14:18:22 +0300

    2: Added Fractal material, Cube mesh, components

--- Basics-ConstructingAFractal/Assets/Fractal.cs
+++ Basics-ConstructingAFractal/Assets/Fractal.cs
@@ -1,4 +1,11 @@
 ﻿using UnityEngine;
 
 public class Fractal : MonoBehaviour {
+    [SerializeField] Mesh mesh = default;
+    [SerializeField] Material material = default;
+
+    void Start() {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+    }
 }
