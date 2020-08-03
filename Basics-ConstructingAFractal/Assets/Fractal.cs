8658777d91076796c0e2b2d87757d315a6550a51
RunninglVlan <redcapshortcut@gmail.com>
2020-08-03 14:29:54 +0300

    3: Added Fractal children, copied/set fields

--- Basics-ConstructingAFractal/Assets/Fractal.cs
+++ Basics-ConstructingAFractal/Assets/Fractal.cs
@@ -3,9 +3,23 @@
 public class Fractal : MonoBehaviour {
     [SerializeField] Mesh mesh = default;
     [SerializeField] Material material = default;
+    [SerializeField] int maxDepth = 4;
+
+    int depth;
 
     void Start() {
         gameObject.AddComponent<MeshFilter>().mesh = mesh;
         gameObject.AddComponent<MeshRenderer>().material = material;
+        if (depth < maxDepth) {
+            new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this);
+        }
+    }
+
+    void Initialize(Fractal parent) {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxDepth = parent.maxDepth;
+        depth = parent.depth + 1;
+        transform.parent = parent.transform;
     }
 }
