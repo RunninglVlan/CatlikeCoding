7b452de5876de8c6f25bae5e05a2c7ec9dbe1519
RunninglVlan <RedCapShortCut@gmail.com>
2021-11-01 21:04:36 +0200

    2.6 - Added FPS/MS displayMode

--- Basics-MathematicalSurfaces/Assets/Utils/FrameRateCounter.cs
+++ Basics-MathematicalSurfaces/Assets/Utils/FrameRateCounter.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FrameRateCounter : MonoBehaviour {
     [SerializeField] TMP_Text display;
     [SerializeField, Range(0.1f, 2f)] float sampleDuration = 1f;
+    [SerializeField] DisplayMode displayMode = DisplayMode.FPS;
 
     int frames;
     float duration, bestDuration = float.MaxValue, worstDuration;
@@ -19,10 +20,23 @@ public class FrameRateCounter : MonoBehaviour {
         if (!(duration > sampleDuration)) {
             return;
         }
-        display.SetText($"FPS\n{1 / bestDuration:0}\n{frames / duration:0}\n{1 / worstDuration:0}");
+        display.SetText(displayMode == DisplayMode.FPS
+            ? @$"FPS
+{1 / bestDuration:0}
+{frames / duration:0}
+{1 / worstDuration:0}"
+            : @$"MS
+{1000 * bestDuration:F1}
+{1000 * duration / frames:F1}
+{1000 * worstDuration:F1}");
         frames = 0;
         duration = 0;
         bestDuration = float.MaxValue;
         worstDuration = 0;
+    }
+
+    enum DisplayMode {
+        FPS,
+        MS
     }
 }
