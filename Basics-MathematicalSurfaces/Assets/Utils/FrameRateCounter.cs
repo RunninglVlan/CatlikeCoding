3bcd9217e0ce5a95b4dafac9d9ab62fd12eb5047
RunninglVlan <RedCapShortCut@gmail.com>
2021-11-01 20:44:39 +0200

    2.4 - Added average FPS over 1 second

--- Basics-MathematicalSurfaces/Assets/Utils/FrameRateCounter.cs
+++ Basics-MathematicalSurfaces/Assets/Utils/FrameRateCounter.cs
@@ -3,9 +3,21 @@ using UnityEngine;
 
 public class FrameRateCounter : MonoBehaviour {
     [SerializeField] TMP_Text display;
+    [SerializeField, Range(0.1f, 2f)] float sampleDuration = 1f;
+
+    int frames;
+    float duration;
 
     void Update() {
         var frameDuration = Time.unscaledDeltaTime;
-        display.SetText($"FPS\n{1 / frameDuration:0}\n000\n000");
+        frames++;
+        duration += frameDuration;
+
+        if (!(duration > sampleDuration)) {
+            return;
+        }
+        display.SetText($"FPS\n{frames / duration:0}\n000\n000");
+        frames = 0;
+        duration = 0;
     }
 }
