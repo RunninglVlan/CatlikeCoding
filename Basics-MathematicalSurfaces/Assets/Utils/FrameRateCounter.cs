795dc35e05ee20fe849e78f55d30cfe4e3808a9b
RunninglVlan <RedCapShortCut@gmail.com>
2021-11-01 20:52:57 +0200

    2.5 - Added min/max FPS

--- Basics-MathematicalSurfaces/Assets/Utils/FrameRateCounter.cs
+++ Basics-MathematicalSurfaces/Assets/Utils/FrameRateCounter.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class FrameRateCounter : MonoBehaviour {
@@ -6,18 +7,22 @@ public class FrameRateCounter : MonoBehaviour {
     [SerializeField, Range(0.1f, 2f)] float sampleDuration = 1f;
 
     int frames;
-    float duration;
+    float duration, bestDuration = float.MaxValue, worstDuration;
 
     void Update() {
         var frameDuration = Time.unscaledDeltaTime;
         frames++;
         duration += frameDuration;
+        bestDuration = math.min(frameDuration, bestDuration);
+        worstDuration = math.max(frameDuration, worstDuration);
 
         if (!(duration > sampleDuration)) {
             return;
         }
-        display.SetText($"FPS\n{frames / duration:0}\n000\n000");
+        display.SetText($"FPS\n{1 / bestDuration:0}\n{frames / duration:0}\n{1 / worstDuration:0}");
         frames = 0;
         duration = 0;
+        bestDuration = float.MaxValue;
+        worstDuration = 0;
     }
 }
