1be6388f4602927c9b55e22c55b60cd380b4fe8b
RunninglVlan <RedCapShortCut@gmail.com>
2021-11-02 17:32:29 +0200

    1.2 - Added Utils, utils.uxml, utils.uss, Info, FrameRateCounter

---
+++ Basics-ComputeShaders/Assets/Utils/FrameRateCounter.cs
@@ -0,0 +1,48 @@
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FrameRateCounter : MonoBehaviour {
+    [SerializeField, Range(0.1f, 2f)] float sampleDuration = 1f;
+    [SerializeField] DisplayMode displayMode = DisplayMode.FPS;
+
+    Label display;
+    int frames;
+    float duration, bestDuration = float.MaxValue, worstDuration;
+
+    void Awake() {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        display = root.Q<Label>("fps");
+    }
+
+    void Update() {
+        var frameDuration = Time.unscaledDeltaTime;
+        frames++;
+        duration += frameDuration;
+        bestDuration = math.min(frameDuration, bestDuration);
+        worstDuration = math.max(frameDuration, worstDuration);
+
+        if (!(duration > sampleDuration)) {
+            return;
+        }
+
+        display.text = displayMode == DisplayMode.FPS
+            ? @$"FPS
+{1 / bestDuration:0}
+{frames / duration:0}
+{1 / worstDuration:0}"
+            : @$"MS
+{1000 * bestDuration:F1}
+{1000 * duration / frames:F1}
+{1000 * worstDuration:F1}";
+        frames = 0;
+        duration = 0;
+        bestDuration = float.MaxValue;
+        worstDuration = 0;
+    }
+
+    enum DisplayMode {
+        FPS,
+        MS
+    }
+}
