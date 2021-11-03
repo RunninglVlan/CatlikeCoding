0ef3b04e57f5ffaff2cd1f116ebc6e733f518cd7
RunninglVlan <RedCapShortCut@gmail.com>
2021-11-03 23:08:41 +0200

    1.4 - Added orientation, 3rd child, refactoring

--- Basics-Jobs/Assets/Fractal.cs
+++ Basics-Jobs/Assets/Fractal.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
 
 public class Fractal : MonoBehaviour {
+    static readonly (Vector3 direction, Quaternion orientation)[] CHILDREN = {
+        (Vector3.up, Quaternion.identity),
+        (Vector3.right, Quaternion.Euler(0, 0, -90)),
+        (Vector3.left, Quaternion.Euler(0, 0, 90)),
+    };
+
     [SerializeField, Range(1, 8)] int depth = 4;
+
+    static readonly Fractal[] children = new Fractal[CHILDREN.Length];
 
     void Start() {
         name = $"Fractal {depth}";
@@ -9,17 +17,22 @@ public class Fractal : MonoBehaviour {
             return;
         }
 
-        var child1 = CreateChild(Vector3.right);
-        var child2 = CreateChild(Vector3.up);
-        child1.transform.SetParent(transform, false);
-        child2.transform.SetParent(transform, false);
+        for (var index = 0; index < CHILDREN.Length; index++) {
+            var (direction, orientation) = CHILDREN[index];
+            children[index] = CreateChild(direction, orientation);
+        }
 
-        Fractal CreateChild(Vector3 direction) {
+        foreach (var child in children) {
+            child.transform.SetParent(transform, false);
+        }
+
+        Fractal CreateChild(Vector3 direction, Quaternion orientation) {
             var child = Instantiate(this);
             child.depth = depth - 1;
             var childTransform = child.transform;
             childTransform.localPosition = direction * .75f;
             childTransform.localScale = Vector3.one * .5f;
+            childTransform.localRotation = orientation;
             return child;
         }
     }
