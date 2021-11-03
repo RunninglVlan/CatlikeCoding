18b58c1a0f581200387a4a166ad5ef87f089dba2
RunninglVlan <RedCapShortCut@gmail.com>
2021-11-03 22:52:05 +0200

    1.2 - Creating one more child
    Setting parent after creating both children
    - otherwise first is duplicated

--- Basics-Jobs/Assets/Fractal.cs
+++ Basics-Jobs/Assets/Fractal.cs
@@ -9,10 +9,18 @@ public class Fractal : MonoBehaviour {
             return;
         }
 
-        var child = Instantiate(this, transform);
-        child.depth = depth - 1;
-        var childTransform = child.transform;
-        childTransform.localPosition = Vector3.right * .75f;
-        childTransform.localScale = Vector3.one * .5f;
+        var child1 = CreateChild(Vector3.right);
+        var child2 = CreateChild(Vector3.up);
+        child1.transform.SetParent(transform, false);
+        child2.transform.SetParent(transform, false);
+
+        Fractal CreateChild(Vector3 direction) {
+            var child = Instantiate(this);
+            child.depth = depth - 1;
+            var childTransform = child.transform;
+            childTransform.localPosition = direction * .75f;
+            childTransform.localScale = Vector3.one * .5f;
+            return child;
+        }
     }
 }
