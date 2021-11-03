e51b9049477ba51b994c099c7109eb9f851a558e
RunninglVlan <RedCapShortCut@gmail.com>
2021-11-04 01:04:45 +0200

    2.5 - Optimized Fractal is finished except animation
    Set correct material for FractalChild
    Removed Mesh/Material from Scene Fractal

--- Basics-Jobs/Assets/Fractal.cs
+++ Basics-Jobs/Assets/Fractal.cs
@@ -9,7 +9,7 @@ public class Fractal : MonoBehaviour {
         (Vector3.back, Quaternion.Euler(-90, 0, 0))
     };
 
-    const float CHILD_OFFSET = .75f;
+    const float CHILD_OFFSET = 1.5f;
     const float CHILD_SCALE = .5f;
     const float ROTATION_SPEED = 22.5f;
 
@@ -25,28 +25,48 @@ public class Fractal : MonoBehaviour {
         }
 
         float currentScale = 1;
-        CreateChild(0, 0, currentScale);
+        children[0][0] = CreateChild(0, 0, currentScale);
         for (var level = 1; level < children.Length; level++) {
             currentScale *= CHILD_SCALE;
             var levelParts = children[level];
             for (var part = 0; part < levelParts.Length; part += CHILDREN.Length) {
                 for (var child = 0; child < CHILDREN.Length; child++) {
-                    CreateChild(level, child, currentScale);
+                    levelParts[part + child] = CreateChild(level, child, currentScale);
                 }
             }
         }
 
-        void CreateChild(int level, int index, float scale /*Vector3 direction, Quaternion orientation*/) {
+        Child CreateChild(int level, int index, float scale) {
             var child = Instantiate(childPrefab, transform);
             child.name = $"Fractal [{level}, {index}]";
             var childTransform = child.transform;
-            // childTransform.localPosition = direction * CHILD_OFFSET;
             childTransform.localScale = Vector3.one * scale;
-            // childTransform.localRotation = orientation;
+            var (direction, orientation) = CHILDREN[index];
+            return new Child {
+                direction = direction,
+                orientation = orientation,
+                transform = child
+            };
         }
     }
 
-    // void Update() => transform.Rotate(0, ROTATION_SPEED * Time.deltaTime, 0);
+    void Update() {
+        // transform.Rotate(0, ROTATION_SPEED * Time.deltaTime, 0);
+        for (var level = 1; level < children.Length; level++) {
+            var parents = children[level - 1];
+            var levelChildren = children[level];
+            for (var index = 0; index < levelChildren.Length; index++) {
+                var parentTransform = parents[index / CHILDREN.Length].transform;
+                var child = levelChildren[index];
+                var childTransform = child.transform;
+                var parentRotation = parentTransform.localRotation;
+                childTransform.localRotation = parentRotation * child.orientation;
+                childTransform.localPosition =
+                    parentTransform.localPosition +
+                    parentRotation * child.direction * (childTransform.localScale.x * CHILD_OFFSET);
+            }
+        }
+    }
 
     struct Child {
         public Vector3 direction;
