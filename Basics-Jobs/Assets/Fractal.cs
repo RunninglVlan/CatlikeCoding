e4525c59fd55bfd561e286062f0a9334143cdc69
RunninglVlan <RedCapShortCut@gmail.com>
2021-11-03 22:38:30 +0200

    1.1 - Creating child Fractals in Start of certain depth

--- Basics-Jobs/Assets/Fractal.cs
+++ Basics-Jobs/Assets/Fractal.cs
@@ -2,4 +2,17 @@ using UnityEngine;
 
 public class Fractal : MonoBehaviour {
     [SerializeField, Range(1, 8)] int depth = 4;
+
+    void Start() {
+        name = $"Fractal {depth}";
+        if (depth <= 1) {
+            return;
+        }
+
+        var child = Instantiate(this, transform);
+        child.depth = depth - 1;
+        var childTransform = child.transform;
+        childTransform.localPosition = Vector3.right * .75f;
+        childTransform.localScale = Vector3.one * .5f;
+    }
 }
