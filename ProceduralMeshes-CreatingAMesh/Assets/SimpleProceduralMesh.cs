d39b37ee2e30a3e592295f30ccdc8c4c396be405
RunninglVlan <RedCapShortCut@gmail.com>
2021-11-09 15:03:34 +0200

    2.2 - Reusing vertices, removed duplicate data

--- ProceduralMeshes-CreatingAMesh/Assets/SimpleProceduralMesh.cs
+++ ProceduralMeshes-CreatingAMesh/Assets/SimpleProceduralMesh.cs
@@ -5,25 +5,15 @@ public class SimpleProceduralMesh : MonoBehaviour {
     void OnEnable() {
         var mesh = new Mesh {
             name = "ProceduralMesh",
-            vertices = new[] {
-                Vector3.zero, Vector3.up, Vector3.right,
-                new Vector3(0, 1.1f, 0), new Vector3(1.1f, 1.1f, 0), new Vector3(1.1f, 0, 0)
-            },
-            normals = new[] {
-                Vector3.back, Vector3.back, Vector3.back,
-                Vector3.back, Vector3.back, Vector3.back
-            },
+            vertices = new[] { Vector3.zero, Vector3.up, new Vector3(1, 1, 0), Vector3.right },
+            normals = new[] { Vector3.back, Vector3.back, Vector3.back, Vector3.back },
             tangents = new[] {
-                new Vector4(1, 0, 0, -1), new Vector4(1, 0, 0, -1), new Vector4(1, 0, 0, -1),
-                new Vector4(1, 0, 0, -1), new Vector4(1, 0, 0, -1), new Vector4(1, 0, 0, -1)
+                new Vector4(1, 0, 0, -1), new Vector4(1, 0, 0, -1), new Vector4(1, 0, 0, -1), new Vector4(1, 0, 0, -1)
             },
-            uv = new[] {
-                Vector2.zero, Vector2.up, Vector2.right,
-                Vector2.up, Vector2.one, Vector2.right
-            },
+            uv = new[] { Vector2.zero, Vector2.up, Vector2.one, Vector2.right },
             triangles = new[] {
-                0, 1, 2,
-                3, 4, 5
+                0, 1, 3,
+                3, 1, 2
             }
         };
         GetComponent<MeshFilter>().mesh = mesh;
