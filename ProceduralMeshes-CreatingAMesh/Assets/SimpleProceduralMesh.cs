a657dbd36479072325e953f2d0c61e0372e51283
RunninglVlan <RedCapShortCut@gmail.com>
2021-11-09 13:59:24 +0200

    1.3 - Added Mesh vertices/triangles, CW winding order is front of Mesh

--- ProceduralMeshes-CreatingAMesh/Assets/SimpleProceduralMesh.cs
+++ ProceduralMeshes-CreatingAMesh/Assets/SimpleProceduralMesh.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class SimpleProceduralMesh : MonoBehaviour {
     void Awake() {
-        var mesh = new Mesh { name = "ProceduralMesh" };
+        var mesh = new Mesh {
+            name = "ProceduralMesh",
+            vertices = new[] { Vector3.zero, Vector3.up, Vector3.right },
+            triangles = new[] { 0, 1, 2 }
+        };
         GetComponent<MeshFilter>().mesh = mesh;
     }
 }
