fe14a070e956bb58138f9b4d8ac9a9c8b2034648
RunninglVlan <RedCapShortCut@gmail.com>
2021-11-09 16:46:39 +0200

    3.2 - Started using advanced Mesh API, created MeshData

--- ProceduralMeshes-CreatingAMesh/Assets/AdvancedMultiStreamProceduralMesh.cs
+++ ProceduralMeshes-CreatingAMesh/Assets/AdvancedMultiStreamProceduralMesh.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class AdvancedMultiStreamProceduralMesh : MonoBehaviour {
     void OnEnable() {
+        var meshDataArray = Mesh.AllocateWritableMeshData(1);
+        var meshData = meshDataArray[0];
+
         var mesh = new Mesh { name = "ProceduralMesh" };
+        Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, mesh);
         GetComponent<MeshFilter>().mesh = mesh;
     }
 }
