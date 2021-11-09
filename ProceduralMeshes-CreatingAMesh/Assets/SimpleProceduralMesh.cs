dcfd3bffe7a2ddd885e50bd92448c996e10be278
RunninglVlan <RedCapShortCut@gmail.com>
2021-11-09 14:59:11 +0200

    2.1 - Added one more triangle, added some space between them

--- ProceduralMeshes-CreatingAMesh/Assets/SimpleProceduralMesh.cs
+++ ProceduralMeshes-CreatingAMesh/Assets/SimpleProceduralMesh.cs
@@ -5,11 +5,26 @@ public class SimpleProceduralMesh : MonoBehaviour {
     void OnEnable() {
         var mesh = new Mesh {
             name = "ProceduralMesh",
-            vertices = new[] { Vector3.zero, Vector3.up, Vector3.right },
-            normals = new[] { Vector3.back, Vector3.back, Vector3.back },
-            tangents = new[] { new Vector4(1, 0, 0, -1), new Vector4(1, 0, 0, -1), new Vector4(1, 0, 0, -1) },
-            uv = new[] { Vector2.zero, Vector2.up, Vector2.right },
-            triangles = new[] { 0, 1, 2 }
+            vertices = new[] {
+                Vector3.zero, Vector3.up, Vector3.right,
+                new Vector3(0, 1.1f, 0), new Vector3(1.1f, 1.1f, 0), new Vector3(1.1f, 0, 0)
+            },
+            normals = new[] {
+                Vector3.back, Vector3.back, Vector3.back,
+                Vector3.back, Vector3.back, Vector3.back
+            },
+            tangents = new[] {
+                new Vector4(1, 0, 0, -1), new Vector4(1, 0, 0, -1), new Vector4(1, 0, 0, -1),
+                new Vector4(1, 0, 0, -1), new Vector4(1, 0, 0, -1), new Vector4(1, 0, 0, -1)
+            },
+            uv = new[] {
+                Vector2.zero, Vector2.up, Vector2.right,
+                Vector2.up, Vector2.one, Vector2.right
+            },
+            triangles = new[] {
+                0, 1, 2,
+                3, 4, 5
+            }
         };
         GetComponent<MeshFilter>().mesh = mesh;
     }
