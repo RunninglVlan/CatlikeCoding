efc6454708227ed1a30f5851a19c6c98244e6342
RunninglVlan <RedCapShortCut@gmail.com>
2021-11-09 14:43:54 +0200

    1.6 - Added tangents and normal texture

--- ProceduralMeshes-CreatingAMesh/Assets/SimpleProceduralMesh.cs
+++ ProceduralMeshes-CreatingAMesh/Assets/SimpleProceduralMesh.cs
@@ -7,6 +7,7 @@ public class SimpleProceduralMesh : MonoBehaviour {
             name = "ProceduralMesh",
             vertices = new[] { Vector3.zero, Vector3.up, Vector3.right },
             normals = new[] { Vector3.back, Vector3.back, Vector3.back },
+            tangents = new[] { new Vector4(1, 0, 0, -1), new Vector4(1, 0, 0, -1), new Vector4(1, 0, 0, -1) },
             uv = new[] { Vector2.zero, Vector2.up, Vector2.right },
             triangles = new[] { 0, 1, 2 }
         };
